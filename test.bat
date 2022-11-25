7c986ad5f70abfec4c3f2b185a0316c818f7a7ac
fahadharoon <fahadharoon11@hotmail.com>
2022-11-25 22:04:20 +0500

    Update and rename sed.bat to test.bat

--- test.bat
+++ test.bat
@@ -2,4 +2,4 @@ sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=0.0.0.0/1" "
 sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=0.0.0.1/2" "E:\SED\test.INI"
 sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=0.0.0.2/3" "E:\SED\test.INI"
 sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=0.0.0.3/4" "E:\SED\test.INI"
-exit
+
