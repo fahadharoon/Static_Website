78e8f63b7d6a4a2398a90aec93183991b50f0a67
fahadharoon <fahadharoon11@hotmail.com>
2022-12-16 20:48:26 +0500

    Delete TEST directory

--- TEST/test.bat
+++
@@ -1,7 +0,0 @@
-sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=192.168.1.1/1" "E:\SED\test.INI"
-sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=192.168.1.2/2" "E:\SED\test.INI"
-sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=192.168.1.3/3" "E:\SED\test.INI"
-sed -i ":a;N;$!ba;s/ServerIP=[0-9]*\.[0-9]*\.[0-9]*\.[0-9]/ServerIP=192.168.1.4/4" "E:\SED\test.INI"
-
-
-
